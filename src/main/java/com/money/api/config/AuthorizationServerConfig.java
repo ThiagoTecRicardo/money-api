06080b574ec2bdff92328fda154a8d85108dcfdc
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-08-12 17:52:12 -0300

    Adding name user on token

--- src/main/java/com/money/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/money/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.money.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.money.api.config.token.CustomTokenEnhancer;
 
 
 @SuppressWarnings("deprecation")
@@ -42,6 +48,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			
+			TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+			tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
+			
 			endpoints
 				.authenticationManager(authenticationManager)
 				.userDetailsService(userDetailsService)
@@ -63,6 +74,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		public TokenStore tokenStore() {
 			return new JwtTokenStore(accessTokenConverter());
 		}
+		
+		@Bean
+		public TokenEnhancer tokenEnhancer() {
+			return new CustomTokenEnhancer();
+		}	
 		
 		
 		
