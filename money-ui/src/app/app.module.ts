35ac20ec6dc52a993de54b3fd138c810d3fb9f02
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-08-25 23:35:03 -0300

    add botao de selecao

--- money-ui/src/app/app.module.ts
+++ money-ui/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 import { AppComponent } from './app.component';
 import {TooltipModule} from 'primeng/tooltip';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { CampoColoridoDirective } from './campo-colorido.directive';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+
+
+
 
 
 @NgModule({
@@ -20,17 +27,22 @@ import { CampoColoridoDirective } from './campo-colorido.directive';
     LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
-    CampoColoridoDirective
+    LancamentoCadastroComponent
+
 
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
 
     ButtonModule,
     InputTextModule,
+    InputTextareaModule,
     TableModule,
     TabViewModule,
     TooltipModule,
+    CalendarModule,
+    SelectButtonModule
 
   ],
   providers: [],
