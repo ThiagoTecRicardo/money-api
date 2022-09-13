7df0db8c9fb3e8dedee6b6c70cd2a3e06a351d94
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-13 01:55:05 -0300

    Configurando a paginação lazy do PrimeNG

--- money-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ money-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api';
 import { LancamentoService, LancamentoFiltro } from '../lancamento.service';
 
 
@@ -9,21 +10,30 @@ import { LancamentoService, LancamentoFiltro } from '../lancamento.service';
 })
 export class LancamentosPesquisaComponent implements OnInit{
 
+  totalRegistro = 0;
   filtro = new LancamentoFiltro();
   lancamentos: any[] = [];
 
   constructor(private lancamentoService: LancamentoService){}
 
   ngOnInit(): void {
-    this.pesquisar();
+    //this.pesquisar();
   }
 
-  pesquisar(): void {
+  pesquisar(pagina = 0) {
+
+    this.filtro.pagina = pagina;
 
     this.lancamentoService.pesquisar(this.filtro)
       .then(resultado => {
+        this.totalRegistro = resultado.total;
         this.lancamentos = resultado.lancamentos;
       });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first! / event.rows!;
+    this.pesquisar(pagina);
   }
 
 }
