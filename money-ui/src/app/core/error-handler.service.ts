85f2af4d9108a12fe30a02f57028ecc6fc5d6876
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-14 00:08:36 -0300

    mensagem de erro de usuário na exclusão de pessoa

--- money-ui/src/app/core/error-handler.service.ts
+++ money-ui/src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
@@ -17,12 +18,22 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+
+    } else if (errorResponse instanceof HttpErrorResponse
+        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        msg = errorResponse.error[0].mensagemUsuario
+      } catch (e) { }
+
+      console.error('Ocorreu um erro', errorResponse);
+
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
     }
 
-    this.messageService.add({ severity: 'error', detail: msg });
-
+    this.messageService.add({ severity:'error', detail: msg });
   }
 }
