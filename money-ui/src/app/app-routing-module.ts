9b13f3482c517f42f12574dcc368b5f28ab6076c
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-16 21:19:11 -0300

    correcao das rotas

--- money-ui/src/app/app-routing-module.ts
+++ money-ui/src/app/app-routing-module.ts
@@ -1,25 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PaginaNaoEncontradaComponent } from "./core/pagina-nao-encontrada.component";
-import { LancamentoCadastroComponent } from "./lancamentos/lancamento-cadastro/lancamento-cadastro.component";
-import { LancamentosPesquisaComponent } from "./lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component";
-import { CadastroPessoalComponent } from "./pessoas/cadastro-pessoa/cadastro-pessoa.componen";
-import { PessoasPesquisaComponent } from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
-import { LoginFormComponent } from './seguranca/login-form/login-form.component';
+
 
 const routes: Routes = [
-  {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
-  {path: 'lancamentos', component: LancamentosPesquisaComponent},
-  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-  {path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
-  {path: 'pessoas', component: PessoasPesquisaComponent},
-  {path: 'pessoas/nova', component: CadastroPessoalComponent},
-  {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  {path: '**', redirectTo: 'pagina-nao-encontrada'},
-  // {path: 'login', component: LoginFormComponent}
-
-]
-
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
+  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
+];
 @NgModule({
 
   imports: [
--- money-ui/src/app/app.module.ts
+++ money-ui/src/app/app.module.ts
@@ -29,19 +29,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 }
 registerLocaleData(localePt);
 
-const routes: Routes = [
-  {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
-  {path: 'lancamentos', component: LancamentosPesquisaComponent},
-  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
-  {path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
-  {path: 'pessoas', component: PessoasPesquisaComponent},
-  {path: 'pessoas/nova', component: CadastroPessoalComponent},
-  {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  {path: '**', redirectTo: 'pagina-nao-encontrada'},
 
-
-
-]
 
 @NgModule({
   declarations: [
@@ -62,7 +50,7 @@ TranslateModule.forRoot({
 
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
+
 
     LancamentosModule,
     PessoasModule,
