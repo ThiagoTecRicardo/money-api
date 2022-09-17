0e1da53511ffd437e10abc53ac5a123b43007d14
Thiago Ricardo <85974419+ThiagoTecRicardo@users.noreply.github.com>
2022-09-17 16:34:05 -0300

    Delete money-ui directory

--- money-ui/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://thiago-money-api.herokuapp.com',
-  tokenAllowedDomains: [ /thiago-money-api.herokuapp.com/ ],
-  tokenDisallowedRoutes: [/\/oauth\/token/],
-};
