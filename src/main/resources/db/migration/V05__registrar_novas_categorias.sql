8a4d4aa29d3f4106a8e87ec2ffe0524d2e3fa726
Thiago Ricardo <85974419+ThiagoTecRicardo@users.noreply.github.com>
2022-09-17 19:50:05 -0300

    Create V05__registrar_novas_categorias.sql

---
+++ src/main/resources/db/migration/V05__registrar_novas_categorias.sql
@@ -0,0 +1,8 @@
+INSERT INTO categoria (nome) values ('Restaurante');
+INSERT INTO categoria (nome) values ('Recebivies');
+INSERT INTO categoria (nome) values ('Presentes');
+INSERT INTO categoria (nome) values ('Bleleza');
+INSERT INTO categoria (nome) values ('Impostos');
+INSERT INTO categoria (nome) values ('Pet Shop');
+INSERT INTO categoria (nome) values ('Emprestimos/Financiamentos');
+INSERT INTO categoria (nome) values ('Contas de consumo');
