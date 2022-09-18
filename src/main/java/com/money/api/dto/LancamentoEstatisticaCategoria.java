d993a4d3c9541305d095ea734a9db7118493d76c
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-18 02:21:51 -0300

    target/classes/com/money/api/config/property/MoneyApiProperty.class

---
+++ src/main/java/com/money/api/dto/LancamentoEstatisticaCategoria.java
@@ -0,0 +1,34 @@
+package com.money.api.dto;
+
+import com.money.api.model.Categoria;
+
+import java.math.BigDecimal;
+
+public class LancamentoEstatisticaCategoria {
+
+	private Categoria categoria;
+
+	private BigDecimal total;
+
+	public LancamentoEstatisticaCategoria(Categoria categoria, BigDecimal total) {
+		this.categoria = categoria;
+		this.total = total;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+}
