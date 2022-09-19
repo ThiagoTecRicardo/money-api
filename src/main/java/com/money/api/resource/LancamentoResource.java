8028a26ea279317211f2785e9dd424780b891ccc
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-18 22:57:55 -0300

    Retornando os dados estatísticos de lançamento por categoria

--- src/main/java/com/money/api/resource/LancamentoResource.java
+++ src/main/java/com/money/api/resource/LancamentoResource.java
@@ -1,5 +1,6 @@
 package com.money.api.resource;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.money.api.dto.LancamentoEstatisticaCategoria;
 import com.money.api.event.RecursoCriadoEvent;
 import com.money.api.exceptionhandler.MoneyExceptionHandler.Erro;
 import com.money.api.model.Lancamento;
@@ -50,6 +52,12 @@ public class LancamentoResource {
     
 	@Autowired
 	private MessageSource messageSource;
+	
+	@GetMapping("/estatisticas/por-categoria")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and hasAuthority('SCOPE_read')")
+	public List<LancamentoEstatisticaCategoria> porCategoaria(){
+		return this.lancamentoRepository.porCategoria(LocalDate.now());
+	}
 	
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and hasAuthority('SCOPE_read')")
