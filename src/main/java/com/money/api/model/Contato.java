9c31d5edb0b58106209ac311433b86002c175211
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-21 00:12:02 -0300

    Criando a entidade Contato para suportar mestre-detalhe

--- src/main/java/com/money/api/model/Contato.java
+++ src/main/java/com/money/api/model/Contato.java
@@ -1,41 +1,40 @@
 package com.money.api.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
-@Table(name = "pessoa")
-public class Pessoa {
-
+@Table(name = "contato")
+public class Contato {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-
-	@NotNull
-	private String nome;
-
-	@Embedded
-	private Endereco endereco;
-
-	@NotNull
-	private Boolean ativo;
 	
-	@Valid
-	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
-	private List<Contato> contatos;	
+	@NotEmpty
+	private String nome;
+	
+	@Email
+	@NotNull
+	private String email;
+	
+	@NotEmpty
+	private String telefone;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "codigo_pessoa")
+	private Pessoa pessoa;
 
 	public Long getCodigo() {
 		return codigo;
@@ -43,14 +42,6 @@ public class Pessoa {
 
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
-	}
-	
-	public List<Contato> getContatos() {
-		return contatos;
-	}
-
-	public void setContatos(List<Contato> contatos) {
-		this.contatos = contatos;
 	}
 
 	public String getNome() {
@@ -61,26 +52,28 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public Endereco getEndereco() {
-		return endereco;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public Boolean getAtivo() {
-		return ativo;
+	public String getTelefone() {
+		return telefone;
 	}
 
-	public void setAtivo(Boolean ativo) {
-		this.ativo = ativo;
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
-	
-	@JsonIgnore
-	@Transient
-	public boolean isInativo() {
-		return !this.ativo;
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
 
 	@Override
@@ -99,7 +92,7 @@ public class Pessoa {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Pessoa other = (Pessoa) obj;
+		Contato other = (Contato) obj;
 		if (codigo == null) {
 			if (other.codigo != null)
 				return false;
@@ -107,6 +100,5 @@ public class Pessoa {
 			return false;
 		return true;
 	}
-	
-    
+
 }
--- src/main/java/com/money/api/service/LancamentoService.java
+++ src/main/java/com/money/api/service/LancamentoService.java
@@ -49,8 +49,8 @@ public class LancamentoService {
 	@Autowired
 	private Mailer mailer;
 	
-	@Scheduled(fixedDelay = 1000 * 60 * 30)
-	//@Scheduled(cron = "0 0 6 * * *")
+	//@Scheduled(fixedDelay = 1000 * 60 * 30)
+	@Scheduled(cron = "0 0 6 * * *")
 	public void avisarSobreLancamentosVencidos() {
 		
 		if (logger.isDebugEnabled()) {
