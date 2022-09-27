a09a35c0a3ce7f3a65d892c2f521f00cb3db0569
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-27 19:41:24 -0300

    Configurando URL do anexo

--- src/main/java/com/money/api/MoneyApiApplication.java
+++ src/main/java/com/money/api/MoneyApiApplication.java
@@ -3,15 +3,22 @@ package com.money.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 import com.money.api.config.property.MoneyApiProperty;
 
 @SpringBootApplication
 @EnableConfigurationProperties(MoneyApiProperty.class )
 public class MoneyApiApplication {
+	
+	private static ApplicationContext APPLICATION_CONTEXT;
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoneyApiApplication.class, args);
+		APPLICATION_CONTEXT = SpringApplication.run(MoneyApiApplication.class, args);
+	}
+	
+	public static <T> T getBean(Class<T> type) {
+		return APPLICATION_CONTEXT.getBean(type);
 	}
 
 }
--- src/main/java/com/money/api/model/Lancamento.java
+++ src/main/java/com/money/api/model/Lancamento.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -18,7 +19,9 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.money.api.repository.listener.LancamentoAnexoListener;
 
+@EntityListeners(LancamentoAnexoListener.class)
 @Entity
 @Table(name = "lancamento")
 public class Lancamento {
